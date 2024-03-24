dd99bf7b9d12a3f560878d2c00b7c9d3de696110
Afraym <afraym100herz@gmail.com>
2024-03-24 16:37:59 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,134 +8,113 @@ class BussinesCardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-      Icons.arrow_back,
-      color: Colors.white,
-      size: 18,
-      
-    ), 
-        centerTitle: true,
-        backgroundColor: Color(0xff0A0C23),
-        title: const Text(
-          "BMI CALCULATOR",
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 10,
-              backgroundColor: Color(0xff0A0C23)),
-        ),
-      ),
-      backgroundColor: Color(0xff0A0C23),
+      backgroundColor: Colors.white,
       body: Column(
-        
+        crossAxisAlignment: CrossAxisAlignment.center ,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Center(
-            child: Container(
-                child: Text(
-              "Your Result",
-              style: TextStyle(color: Colors.white , fontSize: 28),
-            )),
+          CircleAvatar(
+            radius: 70,
+            backgroundColor: Colors.white,
+            child: Icon(Icons.phone_iphone , size: 80  , color: Colors.red,),
+            
           ),
-          
-             Padding(
-               padding: const EdgeInsets.only(top: 12, bottom: 20 , left: 12 , right: 12),
-               child: Container(
-                width: 200,
-                height: 300,
-                color: Color.fromARGB(111, 26, 22, 77) ,
-                child : const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+
+          SizedBox(
+            height: 50,
+            child: Text(
+              "Say Hello To Your New App!",
+              style: TextStyle(
+                  color: Colors.red,
+                  backgroundColor: Colors.white,
+                  fontSize: 18,
+                  fontFamily: 'Pacifico'),
+            ),
+          ),
+          Text(
+            "You've just saved a week of development and headaches.",
+            style: TextStyle(
+                color: Colors.black,
+                backgroundColor: Colors.white,
+                fontSize: 10),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top : 15 , right: 10 , left: 10 , bottom: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8), color: Colors.white),
+              height: 50,
+              
+              child: Row(
+               mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                     Padding(
-                       padding: const EdgeInsets.only(bottom:13 , top: 0 ),
-                       child: Text("Male",style : TextStyle(color: Colors.white, fontSize: 19)),
-                     )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                     Padding(
-                       padding: const EdgeInsets.all(13),
-                       child: Text("Normal",style : TextStyle(color: Colors.green, fontSize: 19)),
-                     )
-                    ],
-                  ) ,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                     Padding(
-                       padding: const EdgeInsets.all(13),
-                       child: Text("22",style : TextStyle(color: Colors.white, fontSize: 25)),
-                     )
-                    ],
+                  
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10 ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration( color: Colors.red , borderRadius: BorderRadius.circular(15) ),
+                          
+                          height: 50,
+                          width: 200,
+                          child: Center(
+                            child: Text(
+                              'log in',
+                              
+                              style: TextStyle(backgroundColor: Colors.red, color: Colors.white , fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   
-                     Padding(
-                       padding: const EdgeInsets.only(bottom:1 , top: 13),
-                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                         children: [
-                           Text("Normal BMI range:",style : TextStyle(color: Colors.white, fontSize: 8)),
-                         ],
-                       ),
-                     ),
-                     Padding(
-                       padding: const EdgeInsets.only(bottom:13),
-                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                         children: [
-                           Text("18.5-24.9 kg/m^2",style : TextStyle(color: Colors.white, fontSize: 8)),
-                         ],
-                       ),
-                     ),
-                     Padding(
-                       padding: const EdgeInsets.only(bottom:1 , top: 13),
-                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                         children: [
-                           Text("You have a normal",style : TextStyle(color: Colors.green, fontSize: 12)),
-                         ],
-                       ),
-                     ),
-                     Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                         children: [
-                           Text("body weight.Good job",style : TextStyle(color: Colors.green, fontSize: 12)),
-                         ],
-                       ),
-                     
-               
-                        
-               
-                    ],
-                
-                )
-                           ),
-             ),
-
-          Container(
-            color: Colors.pink,
-            height: 50,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center ,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Re-Calculate Your BMI" , style : TextStyle(color: Colors.white, fontSize: 12)),
-              ],
+                ],
+              ),
             ),
-          )
+          ) ,
+           Padding(
+            padding: EdgeInsets.only(top : 5 , right: 10 , left: 10 , bottom: 5),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8), color: Colors.white),
+              height: 50,
+              
+              child: Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10 ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration( color: Colors.red , borderRadius: BorderRadius.circular(15) ),
+                          
+                          height: 50,
+                          width: 200,
+                          child: Center(
+                            child: Text(
+                              'Sign up',
+                              
+                              style: TextStyle(backgroundColor: Colors.red, color: Colors.white , fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ) ,
+       
         ],
       ),
     ));
